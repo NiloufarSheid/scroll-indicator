62b64a776e40f78fa19667196632e996d52eb533
NiloufarSheyd <155656698+NiloufarSheid@users.noreply.github.com>
2024-07-21 13:04:42 +0330

    add css to project

--- src/Content.jsx
+++ src/Content.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 const Content = () => {
   return (
     <div className='content'>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, ullam.</p>
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim, hic.</p>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi, dolor?</p>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod, laboriosam?</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam reprehenderit magnam voluptas vitae? Amet voluptatibus iusto nisi, temporibus architecto quis animi, non voluptate alias nostrum cupiditate dignissimos, hic repellendus ex.</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus fugiat, facere perspiciatis vero impedit culpa odio error officia neque harum similique quod id non ad dicta sed voluptates accusantium minima!.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque mollitia sit laudantium iure voluptatem, consectetur quis blanditiis ullam vel? Corrupti libero ipsam vitae facere repellat nostrum vero dolores nobis pariatur!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil maxime dicta tempore natus fugit consequatur delectus nesciunt minus fugiat nulla! Repellat, necessitatibus saepe maxime minus dolorum accusantium exercitationem provident, nemo quisquam sint, ipsum ad praesentium suscipit? Nulla accusantium incidunt fuga.</p>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere, officiis!</p>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam, consequatur!</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa quae ratione animi deserunt iure repellat in deleniti ex error, totam earum explicabo facere unde autem temporibus, esse harum blanditiis cumque vel rem eum? Asperiores suscipit, iusto provident tempora doloribus qui dolorum. Molestias, rem laudantium. Laboriosam nobis natus sed facere eligendi?</p>
     </div>
   );
 }
