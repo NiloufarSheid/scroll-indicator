b61b3de702257f4c24b5c75a5b45cbe351dab4e9
NiloufarSheyd <155656698+NiloufarSheid@users.noreply.github.com>
2024-07-21 14:48:47 +0330

    set usestate for scroolindicator

--- src/Indicator.jsx
+++ src/Indicator.jsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Indicator = () => {
+    const[scrollTop,setScrollTop]=useState(0);
+
+    const winScroll=document.documentElement.scrollTop;
+    const height=document.documentElement.scrollHeight-document.documentElement.clientHeight;
+    const scrolled=(winScroll/height)*100;
+    setScrollTop(scrolled)
   return (
     <div className='wrappper'>
         <div className="scrollIndicator">
